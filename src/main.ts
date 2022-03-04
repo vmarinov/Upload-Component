4df9c63b55037af7ef3a028410aca26962d7827a
Ventsislav Marinov <vmarinovrs@gmail.com>
2022-03-04 19:25:11 +0200

    Add ReactiveFormsModule to imports and UploadComponent to delcarations

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,26 @@ import { Input, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Component } from '@angular/core';
+import { UploadComponent } from './uploadComponent.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app',
   template: `
+    <div class="col-md-6">
+      <br>
+      <upload></upload>
+    </div>
   `
 })
 class AppComponent {
 }
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
+    UploadComponent
   ],
   bootstrap: [AppComponent]
 })
