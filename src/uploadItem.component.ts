a6cf9da0720b11a81459dca701388bcf2f956a68
Ventsislav Marinov <vmarinovrs@gmail.com>
2022-03-07 16:19:18 +0200

    added fade animation

--- src/uploadItem.component.ts
+++ src/uploadItem.component.ts
@@ -1,17 +1,34 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
+import {
+    state,
+    style,
+    animate,
+    transition,
+    trigger,
+} from "@angular/animations";
 
 @Component({
     selector: 'upload-item',
-    templateUrl: 'upload_item.template.html'
+    templateUrl: 'upload_item.template.html',
+    animations: [
+        trigger('addRemove', [
+            transition(':leave', [
+                animate('500ms', style({ opacity: 0, transform: 'translateY(0px)' }))
+            ])
+        ])
+    ]
 })
 export class UploadItemComponent {
     @Input() file: any;
     @Output() fileRemoved: EventEmitter<any> = new EventEmitter<any>();
 
-    removed: boolean = false;
+    shown: boolean = true;
 
     removeFile(file: any) {
-        this.removed = true;
-        this.fileRemoved.emit(file);
+        this.shown = false;
+        let timeout = setTimeout(() => {
+            this.fileRemoved.emit(file);
+            clearTimeout(timeout);
+        }, 500);
     }
 }
